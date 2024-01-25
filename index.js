8962e60856195ed9e17d8a7ce8f0baad4b6fae95
Mayk Brito <maykbrito@gmail.com>
2024-01-25 20:00:41 -0300

    perguntas de futebol

--- index.js
+++ index.js
@@ -1,96 +1,98 @@
 const perguntas = [
-    {
-      pergunta: "Qual é a finalidade do comando 'console.log()' em JavaScript?",
-      respostas: [
-        "Exibir uma mensagem de erro",
-        "Imprimir dados no console",
-        "Criar uma variável"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a função do operador '===' em comparações em JavaScript?",
-      respostas: [
-        "Comparação de valores sem considerar o tipo",
-        "Atribuição de valores",
-        "Comparação estrita de valores e tipos"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Como se declara uma variável em JavaScript?",
-      respostas: [
-        "let myVar;",
-        "const myVar = 10;",
-        "ambas as opções acima estão corretas"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "O que é uma função em JavaScript?",
-      respostas: [
-        "Um tipo de dado",
-        "Um bloco de código reutilizável",
-        "Uma variável global"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a diferença entre 'let' e 'const' na declaração de variáveis?",
-      respostas: [
-        "Nenhuma, são sinônimos",
-        "let é usado para valores constantes, const para variáveis",
-        "let permite reatribuição, const cria variáveis imutáveis"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "O que é o DOM em JavaScript?",
-      respostas: [
-        "Um método de criptografia",
-        "Um modelo de objeto para manipular documentos HTML",
-        "Uma linguagem de programação"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Como se realiza uma iteração sobre os elementos de um array em JavaScript?",
-      respostas: [
-        "Usando a estrutura 'if-else'",
-        "Com a declaração 'switch'",
-        "Utilizando loops como 'for' ou 'forEach'"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "O que é o JSON em JavaScript?",
-      respostas: [
-        "Um método de formatação de texto",
-        "Uma linguagem de estilização",
-        "Um formato de dados leve e intercambiável"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Qual é a diferença entre 'null' e 'undefined' em JavaScript?",
-      respostas: [
-        "São iguais, usados de forma intercambiável",
-        "'null' representa a ausência de valor, 'undefined' é atribuído explicitamente",
-        "Ambos representam valores vazios"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Como se adiciona um evento a um elemento HTML usando JavaScript?",
-      respostas: [
-        "Apenas com CSS",
-        "Usando o atributo 'event'",
-        "Através do método 'addEventListener'"
-      ],
-      correta: 2
-    },
-  ];
-  
+  {
+    pergunta: "Qual time brasileiro conquistou a Copa Libertadores da América em 1992?",
+    respostas: [
+      "São Paulo FC",
+      "Flamengo",
+      "Grêmio"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Quem foi o artilheiro da Copa do Mundo de 1994, onde o Brasil foi campeão?",
+    respostas: [
+      "Romário",
+      "Bebeto",
+      "Ronaldo"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Em que ano o Palmeiras conquistou a Copa Rio Internacional?",
+    respostas: [
+      "1997",
+      "1999",
+      "1998"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual clube brasileiro venceu a Supercopa Libertadores em 1993?",
+    respostas: [
+      "Cruzeiro",
+      "Internacional",
+      "Vasco da Gama"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Quem foi o técnico da Seleção Brasileira na Copa do Mundo de 1998?",
+    respostas: [
+      "Vanderlei Luxemburgo",
+      "Carlos Alberto Parreira",
+      "Felipão"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual jogador brasileiro foi eleito o Melhor do Mundo pela FIFA em 1996?",
+    respostas: [
+      "Ronaldo",
+      "Romário",
+      "Rivaldo"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Em 1995, qual clube brasileiro venceu a Taça Conmebol?",
+    respostas: [
+      "Santos FC",
+      "Grêmio",
+      "Fluminense"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual foi o primeiro time a vencer a Copa do Brasil em 1990?",
+    respostas: [
+      "Corinthians",
+      "Flamengo",
+      "Vasco da Gama"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Em 1994, qual jogador brasileiro marcou um gol decisivo nas quartas de final da Copa do Mundo?",
+    respostas: [
+      "Bebeto",
+      "Romário",
+      "Zinho"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual clube brasileiro foi campeão da Copa CONMEBOL em 1998?",
+    respostas: [
+      "Atlético Mineiro",
+      "Corinthians",
+      "Fluminense"
+    ],
+    correta: 0
+  },
+];
+
+
+
   const quiz = document.querySelector('#quiz')
   const template = document.querySelector('template')
   
